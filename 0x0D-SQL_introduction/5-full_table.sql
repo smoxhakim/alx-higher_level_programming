ab9d90459fe979ccbd4a40a3ed14cb54fbcccc0e
smoxhakim <joulal1994@gmail.com>
2024-05-21 15:59:38 +0100

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0 
+-- script that prints the full description of the table first_table
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM information_schema.COLUMNS
