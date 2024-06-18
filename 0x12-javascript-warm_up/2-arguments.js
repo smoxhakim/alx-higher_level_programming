fd46159afb51f4377c6ec2065a9f9b7b59c52ffa
smoxhakim <joulal1994@gmail.com>
2024-06-18 19:43:30 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const argsCount = args.langth - 2;
+const argsCount = args.length - 2;
 
 if (argsCount === 0) {
   console.log('No argument');
 } else if (argsCount === 1) {
   console.log('Argument found');
 } else {
-	console.log('Argument found');
+  console.log('Argument found');
 }
