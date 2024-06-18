ee1154d1c0ea251300deff2c10adf5f2360b6c66
smoxhakim <joulal1994@gmail.com>
2024-06-18 20:52:48 +0100

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,10 +4,10 @@ const args1 = process.argv[2];
 const args2 = process.argv[3];
 const noArg = 'undefined';
 
-if (!args2) {
-  console.log(`${args1} is ${noArg}`);
-} else if (!args1 && !args2) {
+if (!args1 && !args2) {
   console.log(`${noArg} is ${noArg}`);
+} else if (!args2) {
+  console.log(`${args1} is ${noArg}`);
 } else {
   console.log(`${args1} is ${args2}`);
 }
