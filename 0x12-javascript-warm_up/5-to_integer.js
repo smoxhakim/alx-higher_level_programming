50274c1472f7b25fb61a50b13cf50d93ff4bb9cb
smoxhakim <joulal1994@gmail.com>
2024-06-18 21:54:19 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv[2];
+const args = process.argv;
 
 if (!args) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${parseInt(args)}`);
+  console.log(`My number: ${parseInt(args[2])}`);
 }
