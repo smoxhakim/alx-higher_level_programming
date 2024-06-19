c6f6f6202572f1a19db88ea1d45a2bd83bd106a7
smoxhakim <joulal1994@gmail.com>
2024-06-19 23:51:50 +0100

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newArray = [];
+
+  for (let i = list.length - 1, j = 0; i >= 0; j++, i--) {
+    newArray[j] = list[i];
+  }
+
+  return newArray;
+};
