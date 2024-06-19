8f5d51438042cf841a37990c4f2cdc9256aecbe7
smoxhakim <joulal1994@gmail.com>
2024-06-19 20:34:11 +0100

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0; let occur = 0;
+  for (i; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occur++;
+    }
+  }
+  return occur;
+};
