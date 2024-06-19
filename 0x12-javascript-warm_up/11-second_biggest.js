2e27d46b61218ce5c01c5ed87981e58922517884
smoxhakim <joulal1994@gmail.com>
2024-06-19 01:00:01 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
+  const num = process.argv.slice(2).map(Number);
+  const second = num.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
