e1c14c520761f97e821cca253606ba8d6f753144
smoxhakim <joulal1994@gmail.com>
2024-06-22 19:50:42 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""Get all states starting with 'N' and display them."""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states ORDER BY id ASC;"
+        )
+    result = cursor.fetchall()
+
+    for char in result:
+        if char[1][0] == 'N':
+            print(char)
+
+    cursor.close()
+    db.close()
