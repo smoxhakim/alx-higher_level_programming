0f0904ca3677cc2dbed10f7328a8b95e962ac904
smoxhakim <joulal1994@gmail.com>
2024-06-22 19:27:20 +0100

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+"""Get all states and display them."""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    result = cursor.fetchall()
+
+    for item in result:
+        print(item)
+
+    cursor.close()
+    db.close()
