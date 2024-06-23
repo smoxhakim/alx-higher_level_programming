29a0aea42de6d602b12b6e63199cb385d5183174
smoxhakim <joulal1994@gmail.com>
2024-06-23 21:33:23 +0100

    2 files

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""Fetch all States"""
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter_by(id=1).one_or_none()
+    try:
+        print("{}: {}".format(result.id, result.name))
+    except Exception:
+        print("Nothing")
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""Fetch all States"""
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name.like(
+        '%a%')).order_by(State.id.asc()).all()
+
+    for object in result:
+        print("{}: {}".format(object.id, object.name))
