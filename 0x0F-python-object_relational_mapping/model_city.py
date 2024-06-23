c8f041c677154eac9e89eccedf18ef066f11bd1b
smoxhakim <joulal1994@gmail.com>
2024-06-23 23:39:39 +0100

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""City Module"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """City Class"""
+    __tablename__ = "cities"
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False,
+                unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
