d4edc70bf6bb2a8ca5dbb612c575eab15968101a
smoxhakim <joulal1994@gmail.com>
2024-06-23 23:22:38 +0100

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Fetch all States"""
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""Fetch A state"""
+from model_state import Base, State
+from model_city import City
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    result = session.query(City, State).filter(City.state_id == State.id).all()
+
+    for city, state in result:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
