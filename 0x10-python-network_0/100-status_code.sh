affd84dfaab8cf6fc62b5d28c0d21c2e42c193fb
smoxhakim <joulal1994@gmail.com>
2024-07-04 19:16:52 +0100

    100-status_code.sh

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -s -o /tmp/status_code.txt -w "%{http_code}" "$1" & sleep 1 ; cat /tmp/status_code.txt
