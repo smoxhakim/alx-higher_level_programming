8e2fee65abffbcf2775dbfd7b71ff5da4c134a14
smoxhakim <joulal1994@gmail.com>
2024-07-05 19:19:40 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Error code"""
+
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    res = requests.get(url)
+
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)
+
