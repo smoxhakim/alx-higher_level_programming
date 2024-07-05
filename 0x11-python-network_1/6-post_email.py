5aff143eca34177b465935c13d83876a7d4775c1
smoxhakim <joulal1994@gmail.com>
2024-07-05 19:05:51 +0100

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""POST requeste"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    res = requests.post(url, data=data)
+
+    print(res.text)
