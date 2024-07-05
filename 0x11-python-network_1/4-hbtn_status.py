07024d95663940205ef2804715f5fb0fd656464a
smoxhakim <joulal1994@gmail.com>
2024-07-05 18:51:44 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""requests module"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    res = requests.get(url)
+    body = res.text
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
