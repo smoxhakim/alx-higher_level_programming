92a3a2b1684c7995a2922ff2bdfbe41f7fe156f4
smoxhakim <joulal1994@gmail.com>
2024-07-30 16:09:47 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+const fileUpdat = process.argv[3];
+if (!file) {
+  console.error('Please provide a file path as a command-line argument.');
+  process.exit(1);
+}
+
+fs.writeFile(file, fileUpdat, 'utf8', (error) => {
+  if (error) {
+    console.log('Error', error);
+  }
+});
--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
 
 const file = process.argv[2];
+const fileUpdat = process.argv[3]
 if (!file) {
   console.error('Please provide a file path as a command-line argument.');
   process.exit(1);
 }
 
+
+fs.writeFile(file, fileUpdat, 'utf8', (error) => {
+  if (error) {
+    console.log('Error', error)
+  }
+});
 
 fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
