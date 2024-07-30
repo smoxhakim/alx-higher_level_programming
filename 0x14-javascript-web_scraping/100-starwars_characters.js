bc7ed88690364776546d9bf5deac8f2c6bf099b4
smoxhakim <joulal1994@gmail.com>
2024-07-30 19:07:56 +0100

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, (err, response, body) => {
+  if (err) {
+    console.error('Error'.err);
+  }
+  for (const character of body.characters) {
+    request(character, { json: true }, (err, res, body) => {
+      if (err) console.log(err);
+      console.log(body.name);
+    });
+  }
+});
