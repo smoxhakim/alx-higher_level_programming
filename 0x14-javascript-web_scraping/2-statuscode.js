ebdf0e705ba3cbb8ae61e632761e120e7a3f3b06
smoxhakim <joulal1994@gmail.com>
2024-07-30 16:29:24 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
+
 const url = process.argv[2];
+if (!url) {
+  console.error('Please provide a url as a command-line argument.');
+  process.exit(1);
+}
 
 request(url, (err, response) => {
-  if (err) console.log('Error:', err);
-  console.log('code: ', response.statusCode);
+  if (err) {
+    console.log('Error:', err);
+  }
+  console.log('code:', response.statusCode);
 });
