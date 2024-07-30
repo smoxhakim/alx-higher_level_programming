9b0c8f6e1209cc13b17d96ec631563aa86e564d9
smoxhakim <joulal1994@gmail.com>
2024-07-30 19:05:13 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+if (!url) {
+  console.error('Please provide a url as a command-line argument.');
+  process.exit(1);
+}
+
+request(url, { json: true }, (err, response, body) => {
+  if (err) {
+    console.log('Error:', err);
+    return;
+  }
+  const count = {};
+
+  body.forEach(task => {
+    if (task.completed) {
+      if (!count[task.userId]) {
+        count[task.userId] = 0;
+      }
+      count[task.userId]++;
+    }
+  });
+
+  console.log(count);
+});
--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,25 +1,26 @@
 const request = require('request');
-const fs = require('fs');
 
 const url = process.argv[2];
 if (!url) {
   console.error('Please provide a url as a command-line argument.');
   process.exit(1);
 }
-const file = process.argv[3];
-if (!file) {
-  console.error('Please provide a file path as a command-line argument.');
-  process.exit(1);
-}
 
-request(url, (err, response, body) => {
+request(url, { json: true }, (err, response, body) => {
   if (err) {
     console.log('Error:', err);
     return;
   }
-  fs.writeFile(file, body, 'utf8', (err) => {
-    if (err) {
-      console.log('Error:', err);
-    }
+  const count = {};
+  
+  body.forEach( task => {
+    if (task.completed) {
+      if (!count[task.userId]) {
+        count[task.userId] = 0;
+      }
+      count[task.userId]++;
+    }    
+    });
+
+    console.log(count);
   });
-});
