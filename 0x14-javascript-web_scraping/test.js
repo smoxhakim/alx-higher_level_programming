5a2923db952e2231bedacc4a9bac69b06024817b
smoxhakim <joulal1994@gmail.com>
2024-07-30 15:57:35 +0100

    0-readme.js

---
+++ 0x14-javascript-web_scraping/test.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const file = process.argv[2];
+if (!file) {
+  console.error('Please provide a file path as a command-line argument.');
+  process.exit(1);
+}
+
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log(data);
+});
