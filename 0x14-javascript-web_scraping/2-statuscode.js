8c99a92e63d9e7a6a626e241289b547eef6e21ec
smoxhakim <joulal1994@gmail.com>
2024-07-30 16:23:34 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,10 @@
 const request = require('request');
 
 const url = process.argv[2];
+if (!url) {
+  console.error('Please provide a url as a command-line argument.');
+  process.exit(1);
+}
 
 request(url, 'utf8', (err, response) => {
   if (err) {
--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,7 +1,11 @@
 const request = require('request');
-const fs = require('request')
 
 const url = process.argv[2];
+if (!url) {
+  console.error('Please provide a url as a command-line argument.');
+  process.exit(1);
+}
+
 
 request(url, 'utf8', (err, response) => {
   if (err) {
