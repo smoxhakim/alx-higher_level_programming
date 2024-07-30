83c96e6f56fd6d0eb201d100689ca6f6bb018489
smoxhakim <joulal1994@gmail.com>
2024-07-30 16:46:30 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ if (!id) {
   console.error('Please provide a episode number as a command-line argument.');
   process.exit(1);
 }
-const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, { json: true }, (err, response, body) => {
   if (err) {
